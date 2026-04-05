7089187f3d0f03918e2345220e134ca12b3fd49b
husseymarcos <mhussey@mail.austral.edu.ar>
2026-04-05 20:29:05 -0300

    Add dummy test for CI verification

---
+++ test/models/dummy_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class DummyTest < ActiveSupport::TestCase
+  test "dummy test always passes" do
+    assert true
+  end
+
+  test "basic math works" do
+    assert_equal 2, 1 + 1
+  end
+end
